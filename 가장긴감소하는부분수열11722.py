3094ed9c52fa411f5bcd682e9d40abce368692c9
staris <sungdoolim@naver.com>
2020-12-26 00:26:04 +0900

    12/26

---
+++ 가장긴감소하는부분수열11722.py
@@ -0,0 +1,13 @@
+n=int(input())
+ar=list(map(int,input().split()))
+#print(ar)
+result=[1]*n
+
+for i in range(n):
+    r=1
+    for j in range(i+1):
+        if ar[j]>ar[i]:
+            r=max(r,result[j]+1)
+    result[i]=r
+#print(result)
+print(max(result))
