be9ae3ee76f686144a6cd022926c5822b2191020
sungdoolim <sungdoolim@naver.com>
2021-04-04 00:27:31 +0900

    4/4

--- 숨바꼭질 1697.py
+++ 숨바꼭질 1697.py
@@ -5,29 +5,18 @@ ar=[-1]*M
 q=deque([])
 ar[start]=0
 q.append(start)
-count=0
 def bfs():
     while q:
-        #print(q)
         s=q.popleft()
-        #print("s,",s)
-        global count
-        count+=1
-       # print(count)
         if ar[m]!=-1:
             return 0
-
         if s-1>=0 and ar[s-1]==-1:
-
             q.append(s-1)
             ar[s-1]=ar[s]+1
-
         if s+1<M and ar[s+1]==-1:
-
             q.append(s+1)
             ar[s+1]=ar[s]+1
         if s*2<M and ar[s*2]==-1:
-
             q.append(s*2)
             ar[s*2]=ar[s]+1
         
