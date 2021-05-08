2f69288ca1114ed278ef836aaf61264920de6498
sungdoolim <sungdoolim@naver.com>
2021-05-08 22:22:38 +0900

    5/8

--- 단지번호붙이기2667.py
+++ 단지번호붙이기2667.py
@@ -4,32 +4,28 @@ ar=[]
 ckl=[[0]*n for _ in range(n)]
 for _ in range(n):
     ar.append(list(map(int,input())))
-k=0
+num_house=0
 def dfs(i,j):
-    global k
+    global num_house
     if i<0 or j<0 or i>=n or j>=n or ckl[i][j]==1 or ar[i][j]==0:
         return False
         
     else:
-        k+=1
+        num_house+=1
         ckl[i][j]=1
         dfs(i+1,j)
         dfs(i-1,j)
         dfs(i,j+1)
         dfs(i,j-1)
-               
-
-
-
         return True
 count=0
 result=[]
 for i in range(n):
     for j in range(n):
-        k=0
+        num_house=0
         if(dfs(i,j)):
             count+=1
-            result.append(k)
+            result.append(num_house)
 # print(ar)
 # print(ckl)
 result.sort()
